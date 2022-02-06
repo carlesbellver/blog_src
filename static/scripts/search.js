a99ea08e5cd292251a2d81c03ac50e65bad3d85d
Carles Bellver Torlà <carles@carlesbellver.net>
2022-02-06 19:04:26 +0100

    update 2022-02-06 19:04:22

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -17,49 +17,69 @@ function runSearch(q) {
   var results_node = document.getElementById("list_results");
   results_node.innerHTML = "";
   var count = 0;
-  if (q.length > 0) {
+  if (q.length > 3) {
+    var results = [];
+    var terms = q.split(/ +/);
     for (var i = 0; i < archive_results.items.length; i++) {
-    var item = archive_results.items[i];
+      var item = archive_results.items[i];
       var title_lower = item.title.toLowerCase();
       var tags_lower = item.tags.toLowerCase();
       var text_lower = item.content_text.toLowerCase();
-      if (title_lower.includes(q) || tags_lower.includes(q) || text_lower.includes(q)) {
+      score = 0;
+      for (let i = 0; i < terms.length; i++) {
+        if (title_lower.includes(terms[i])) {
+          score += 10;
+        }
+        if (tags_lower.includes(terms[i])) {
+          score += 5;
+        }
+        if (text_lower.includes(terms[i])) {
+          score += 1;
+        }
+      }
+      if (score > 0) {
+        const result = { "url": item.url, "title": item.title, "text": item.content_text, "date": item.date_published, "tags": item.tags, "score": score};
+        results.push(result);
         count++;
-        var p_node = document.createElement("p");        
-        var link_node = document.createElement("a");
-        var d = Date.parse(item.date_published);
-        var date_s = new Date(d).toISOString().substr(0, 10);
-        var date_node = document.createTextNode(date_s); 
-        link_node.appendChild(date_node);
-        link_node.href = item.url;
-        var title_node = null;
-        if (item.title.length > 0) {
-          title_node = document.createElement("span");
-          title_node.innerHTML = ' <a href="'+item.url+'">' + item.title + "</a>"
-          s = item.title + " " + item.content_text;
-        }
-        var s = item.content_text;
-        if (s.length > 200) {
-          s = s.substr(0, 200) + "…";
-        }
-        var text_node = document.createElement("span");
-        text_node.innerHTML = " " + s
-        p_node.appendChild(link_node);
-        /* if (item.photos[0]) { */
-        if (tags_lower.includes("fotos")) {
-          var pic_node = document.createElement("span");
-          pic_node.innerHTML = " &#x1F5BC;"
-          p_node.appendChild(pic_node)
-        }
-        if (title_node != null) {
-          p_node.appendChild(title_node);
-        }
-        p_node.appendChild(text_node);
-        results_node.appendChild(p_node);
       }
     }
   } 
-  if (! count) {
+  if (count) {
+    results.sort( function(a, b) { return b["score"] - a["score"] } );
+    for (let i = 0; i < results.length; i++) {
+      var p_node = document.createElement("p");        
+      var link_node = document.createElement("a");
+      var d = Date.parse(results[i]["date"]);
+      var date_s = new Date(d).toISOString().substr(0, 10);
+      var date_node = document.createTextNode(date_s); 
+      link_node.appendChild(date_node);
+      link_node.href = results[i]["url"];
+      var title_node = null;
+      if (results[i]["title"].length > 0) {
+        title_node = document.createElement("span");
+        title_node.innerHTML = ' <a href="'+results[i]["url"]+'">' + results[i]["title"] + "</a>"
+        s = results[i]["title"] + " " + results[i]["text"];
+      }
+      var s = results[i]["text"];
+      if (s.length > 200) {
+        s = s.substr(0, 200) + "…";
+      }
+      var text_node = document.createElement("span");
+      text_node.innerHTML = " " + s;
+      p_node.appendChild(link_node);
+      if (results[i]["tags"].includes("fotos")) {
+        var pic_node = document.createElement("span");
+        pic_node.innerHTML = " &#x1F5BC;"
+        p_node.appendChild(pic_node);
+      }
+      if (title_node != null) {
+        p_node.appendChild(title_node);
+      }
+      p_node.appendChild(text_node);
+      results_node.appendChild(p_node);
+    }
+  }
+  else {
     results_node.innerHTML = "<p style=\"text-align: center;\">No s'ha trobat res que hi concordi.</p>";
   }
 }
