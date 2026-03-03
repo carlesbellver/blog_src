e089a8fb511d0d1215ca578fa6be1b934266ffe1
Carles Bellver Torlà <bellverc@uji.es>
2026-03-03 17:38:12 +0100

    cleanup

--- static/scripts/microgram.js
+++ static/scripts/microgram.js
@@ -1,72 +1,48 @@
-var max = 250;
+var max = 100;
 
-var container = document.getElementById('microgram');
+var $microgram = document.getElementById('microgram');
 
 function renderImage(image) {
-
-    var linkEl = document.createElement('a');
-    linkEl.href = image['url'];
-    linkEl.className = 'photo-link';
-    linkEl.title = image['date_published'].substring(0, 10);
-    /* linkEl.title = image['content_text']; */
-    container.appendChild(linkEl);
-
-    var imageEl = document.createElement('div');
-    imageEl.className = 'photo';
-    var url = image['image'];
-    imageEl.style.backgroundImage = 'url(' + url + ')';
-    imageEl.innerHTML = "<span>"+image['content_text']+"</span>";
-    
-    /* var textEl = document.createElement('div');
-    textEl.innerHTML = image['content_text'];
-    textEl.className = 'photo-text';
-    imageEl.appendChild(textEl); */
-    
-    linkEl.appendChild(imageEl);
+	var $link = document.createElement('a');
+	$link.href = image['url'];
+	$link.className = 'photo-link';
+	$link.title = image['date_published'].substring(0, 10);
+	/* $link.title = image['content_text']; */
+	$microgram.appendChild($link);
+	var $image = document.createElement('div');
+	$image.className = 'photo';
+	var url = image['image'];
+	$image.style.backgroundImage = 'url(' + url + ')';
+	$image.innerHTML = "<span>"+image['content_text']+"</span>";
+	$link.appendChild($image);
 }
 
 function renderNoContent() {
-    var noPostsEl = document.createElement('p');
-    noPostsEl.innerText = 'No recent photos.';
-    container.appendChild(noPostsEl);
+	var $noPosts = document.createElement('p');
+	$noPosts.innerText = 'Sense fotos recents.';
+	$microgram.appendChild($noPosts);
 }
 
-var hostname = window.location.hostname;
-var domains = hostname.split('.');
-if (domains.length > 2) {
-  domains.shift();
-  hostname = domains.join('.');
-} 
-
-var host = window.location.host;
-var protocol = window.location.protocol;
-/* var jsonUrl = protocol + '//'+ host + '/photos/index.json'; */
-jsonUrl = '/photos/index.json';
-
+var jsonUrl = '/photos/index.json';
 var xhr = new XMLHttpRequest();
 xhr.responseType = "json";
 xhr.open('GET', jsonUrl, true);
 xhr.send();
 
 xhr.onreadystatechange = function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        container.innerHTML = '';
-        if (xhr.response.length == 0) {
-            renderNoContent();
-        } else {
-            var count = 0;
-            xhr.response['items'].forEach(function(image) {
-                if (count < max) {
-                    if (image['image'].indexOf(hostname) != -1) {
-                      renderImage(image);
-                      count++;
-                    }
-                }                
-            });
-            console.log("Fotos: " + count);
-        }
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    $microgram.innerHTML = '';
+    if (xhr.response.length == 0) {
+      renderNoContent();
     }
+    else {
+      var count = 0;
+      xhr.response['items'].forEach(function(image) {
+        if (count < max) {
+          renderImage(image);
+          count++;
+        }        
+      });
+    }
+  }
 }
-
-microgram.innerHTML = '';
-/* microgram.appendChild(container); */
